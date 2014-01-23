9b6ab897166ca8a331584d01cc3fe60da4aa07a1
hArDyTRSi <hardytrsi@gmx.de>
2014-01-23 18:35:45 +0100

    + StartMenu
    - added the most simple StartMenu i could come up with.
    - added some futuristic Fonts

---
+++ Assets/ButtonQuit.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonQuit : MonoBehaviour
+{
+	public float characterSize = 0.42f;
+	public float zoomSize = 0.1f;
+	public float zoomSpeed = 5.0f;
+	
+	private static TextMesh start = null ;
+	private float timer;
+	
+	void Start()
+	{
+		start = this.GetComponent<TextMesh>();
+		
+	}
+	
+	
+	void OnMouseEnter()
+	{
+		timer = 1.5f;
+		
+		//Debug.Log ("MouseEnter");
+	}
+	
+	
+	void OnMouseOver()
+	{
+		timer += zoomSpeed * Time.deltaTime;
+		
+		if(!start)
+			Debug.Log ("No TextMesh found!");
+		else
+			start.characterSize = characterSize + zoomSize * Mathf.Cos(timer);
+		
+		
+		//Debug.Log ("MouseOver");
+	}
+	
+	
+	void OnMouseExit()
+	{
+		start.characterSize = characterSize;
+		
+		Debug.Log ("MouseExit");
+	}
+	
+	
+	void OnMouseDown()
+	{
+		Application.Quit();
+	}
+	
+}
