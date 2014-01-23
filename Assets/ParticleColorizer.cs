1372f41a477558b8fbd527a9b92a429891b26f1e
hArDyTRSi <hardytrsi@gmx.de>
2014-01-23 19:13:48 +0100

    + StartMenuBackgroundFX
    added some effect to the background of the StartMenu

---
+++ Assets/ParticleColorizer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleColorizer : MonoBehaviour {
+
+	private ParticleSystem ps = null;
+
+	void Start ()
+	{
+		ps = this.GetComponent<ParticleSystem>();
+		ps.Play();
+	}
+
+
+	void Update ()
+	{
+/*		Color color = new Color(
+								0.5f + 0.5f*Mathf.Sin(Time.time * 1.0f),
+		                        0.5f + 0.5f*Mathf.Sin(Time.time * 1.1f),
+		                        0.5f + 0.5f*Mathf.Sin(Time.time * 1.2f)
+		                        );
+*/		Color color = new Color(
+								0.75f + 0.25f*Mathf.Sin(Time.time * 1.0f),
+		                        0.75f + 0.25f*Mathf.Cos(Time.time * 1.4f),
+		                        0.75f + 0.25f*Mathf.Sin(Time.time * 1.3f)
+		                        );
+
+		ps.startColor = color;
+	}
+}
